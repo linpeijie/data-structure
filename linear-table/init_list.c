22b54a08cdefa8ba91b5c467ad7dfd23ea3ad9c6
lpj498840744 <lpj498840744@qq.com>
2018-01-20 15:34:53 +0800

    create a linear list

--- linear-table/init_list.c
+++ linear-table/init_list.c
@@ -1,16 +1,17 @@
 #include"def.h"
 
-bool init_list_seq(Seqlist **L){
+bool init_list_seq(Seqlist *L){
     /* create a empty linear list
     */
 
-    L.elem = (int *)malloc(LIST_INIT_SIZE *sizeof(int));
+    (*L).elem = (int *)malloc(LIST_INIT_SIZE *sizeof(int));
     
-    if(!elem)
+    if(!(*L).elem)
         return false;
 
-    L.length = 0;
-    L.listsize = LIST_INIT_SIZE;
-    
+    (*L).length = 0;
+    (*L).listsize = LIST_INIT_SIZE;
+   
     return true;
 }
+
