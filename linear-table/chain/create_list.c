ab9ddef68ba690c7a7d9acfaecd35fe546469377
lpj498840744 <lpj498840744@qq.com>
2018-01-22 14:57:40 +0800

    create a chain table

--- linear-table/chain/create_list.c
+++ linear-table/chain/create_list.c
@@ -1,21 +1,23 @@
 #include"def_c.h"
 
-LinkList create_list(LinkList L){
+/* use '**' Pointers to Pointers to update L
+*/
+LinkList create_list(LinkList *L){
     LNode *s;
     int elem;
 
-    L = (LNode *)malloc(sizeof(LNode));
-    L->next = NULL;
+    (*L) = (LNode *)malloc(sizeof(LNode));
+    (*L)->next = NULL;
 
     printf("Enter the elem of list end with '9999':");
     scanf("%d",&elem);
     while(elem!=9999 ){
         s = (LNode *)malloc(sizeof(LNode));
         s->data = elem;
-        s->next = L->next;
-        L->next = s;
+        s->next = (*L)->next;
+        (*L)->next = s;
         scanf("%d",&elem);
     } 
 
-    return L;
+    return (*L);
 }
