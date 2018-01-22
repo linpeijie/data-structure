58c8b5384a85ccbda853d87e8ea2535ab80fffd9
lpj498840744 <lpj498840744@qq.com>
2018-01-22 15:49:55 +0800

    get elem by position i

---
+++ linear-table/chain/get_elem_c.c
@@ -0,0 +1,17 @@
+#include"def_c.h"
+
+LNode *get_elem_c(LinkList L,int i,LNode **p){
+    int j=1;
+    (*p) = L->next;
+
+    if(i=0) return L;
+
+    if(i<1)  return NULL;
+
+    while((*p) && j<i){
+        (*p) = (*p)->next;
+        ++j;
+    }
+
+    return (*p);
+}
