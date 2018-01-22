249b34f686ab91f218f5508e901693404411cd0b
lpj498840744 <lpj498840744@qq.com>
2018-01-22 14:46:06 +0800

    create a chain table

--- linear-table/chain/create_list.c
+++ linear-table/chain/create_list.c
@@ -1,21 +1,21 @@
 #include"def_c.h"
 
-int create_list(LinkList L){
+LinkList create_list(LinkList L){
     LNode *s;
     int elem;
 
     L = (LNode *)malloc(sizeof(LNode));
     L->next = NULL;
 
-    printf("Enter the elem of list end with 000:");
+    printf("Enter the elem of list end with '9999':");
     scanf("%d",&elem);
-    while(elem!=000){
+    while(elem!=9999 ){
         s = (LNode *)malloc(sizeof(LNode));
         s->data = elem;
         s->next = L->next;
         L->next = s;
         scanf("%d",&elem);
-    }    
+    } 
 
-    return 0;
+    return L;
 }
