1e260bdea4b99bfc30bb01fb077485905198aa43
lpj498840744 <lpj498840744@qq.com>
2018-01-27 12:57:01 +0800

    define

--- stack/def.h
+++ stack/def.h
@@ -1,7 +1,11 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
-typedef struct LinkStack{
-    int elem;
-    struct LinkStack *next;
-}*LinkStack;
+#define STACK_INIT_SIZE 100;
+#define STACKINCREMENT 10;
+
+typedef struct SqStack{
+    int *base;
+    int *top;
+    int stacksize;
+}SqStack;
