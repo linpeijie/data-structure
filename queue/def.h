4e6c7215e1b2e8586d2745c9c13c4c951f237bc1
lpj498840744 <lpj498840744@qq.com>
2018-01-28 18:43:44 +0800

    new

---
+++ queue/def.h
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct QNode{
+    int data;
+    struct QNode* next;
+}QNode,*QueuePtr;
+
+typedef struct{
+    QueuePtr* front;
+    QueuePtr* rear;
+}LinkQueue;
