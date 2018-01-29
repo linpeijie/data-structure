d655c3f85d7618a0a7040650e1fdb26bd9f75e00
lpj498840744 <lpj498840744@qq.com>
2018-01-29 10:19:18 +0800

    destroy a tree

---
+++ tree/destroy_tree.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"def.h"
+
+bool destroy_tree(BiTree T){
+    
+    if(!T){
+        destroy_tree(T->lchild);
+        destroy_tree(T->rchild);
+        free(T);
+        return true;
+    }
+    else
+        return true;
+}
