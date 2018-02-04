65031eae40cf1d2d59f33c8fda489337a1d2019e
lpj498840744 <lpj498840744@qq.com>
2018-02-04 22:56:01 +0800

    print tree

---
+++ tree/print_tree.c
@@ -0,0 +1,30 @@
+#include"def.h"
+
+bool print_tree(BiTree T,char ch[]){
+
+    if(!strcmp(ch,"pre")){
+    /* use 'pre order traverse' print the tree*/
+       if(T!=NULL){
+        printf("%d ",T->elem);
+        print_tree(T->lchild,ch);
+        print_tree(T->rchild,ch);}
+    }
+
+    else if(!strcmp(ch,"in")){
+    /*use 'in order traverse' print the tree*/
+        print_tree(T->lchild,ch);
+        printf("%d ",T->elem);
+        print_tree(T->rchild,ch);
+    }
+
+    else if(!strcmp(ch,"post")){
+    /*use 'post order traverse' print the tree*/
+        print_tree(T->lchild,ch);
+        print_tree(T->rchild,ch);
+        printf("%d ",T->elem);
+    }
+
+    else if(!strcmp(ch,"level")){}
+
+    return true;
+}
