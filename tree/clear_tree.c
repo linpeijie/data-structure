1a965ef740a3787857e68ab4053ee3e7b1d6043d
lpj498840744 <lpj498840744@qq.com>
2018-02-04 22:17:42 +0800

    let the tree empty

--- tree/clear_tree.c
+++ tree/clear_tree.c
@@ -2,13 +2,11 @@
 #include"def.h"
 
 bool clear_tree(BiTree* T){
-    if(!(*T))
-       exit(1);
 
     if((*T)!=NULL){
         clear_tree(&((*T)->lchild));
         clear_tree(&((*T)->rchild));
-        free((*T));
+        (*T)=NULL;
     }
 
     return true;
