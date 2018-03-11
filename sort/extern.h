47d56196e084dd08858284ee7f15da5a7ddd6f92
linpeijie <lpj498840744@qq.com>
2018-03-11 11:47:18 +0800

    add

--- sort/extern.h
+++ sort/extern.h
@@ -3,4 +3,14 @@ int straightInsertSort(int arr[],int n);
 int binaryInsertSort(int arr[],int n);
 int sheelSort(int arr[],int n);
 
+//exchange sort
+int bubbleSort(int arr[],int n);
+int partition(int arr[],int low,int high);
+
+//select sort
+int selectSort(int arr[],int n);
+int heapSort(int arr[],int n);
+
+//merge sort
+int mergeSort(int arr[],int low,int high);
 
--- sort/main.c
+++ sort/main.c
@@ -8,15 +8,14 @@ int main(){
     printf("Please enter the length of array:");
     scanf("%d",&n);
 
-    for(i = 1; i < n+1; i++) {
+    for(i = 0; i < n; i++) {
         scanf("%d",&arr[i]);
     }
 
-    sheelSort(arr,n);
+    mergeSort(arr,0,n-1);
 
-    for(i = 1; i < n+1; i++)
+    for(i = 0; i < n; i++)
         printf("%d ",arr[i]);
-    printf("\n");
 
     return 0;
 }
