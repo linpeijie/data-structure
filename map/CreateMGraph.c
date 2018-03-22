2b878fa5dda32ea6a373cd2c4490c9fc2b74185f
linpeijie <lpj498840744@qq.com>
2018-03-22 13:48:39 +0800

    create a MGraph

---
+++ map/CreateMGraph.c
@@ -0,0 +1,21 @@
+#include"def.h"
+
+void createMGraph(MGraph *G){
+    int i,j,k,w;
+
+    printf("Enter vexnum and arcnum:");
+    scanf("%d%d",&G->vexNum,&G->arcNum);
+
+    printf("Enter the vertex:");
+    for(i = 0; i < G->vexNum; i++)
+        scanf("%d",&G->vex[i]);
+
+    memset(G->vex,INF,sizeof(G->vex));
+    
+    printf("Enter (vi,vj) and weight:\n");
+    for(k = 0; k < G->arcNum; k++){
+        scanf("%d%d%d",&i,&j,&w);
+        G->edge[i][j] = w;
+        G->edge[j][i] = G->edge[i][j];   //symmetric graph
+    }
+}
